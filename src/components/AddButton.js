69dee22f984102bfda140dc027e6a9b2ea1d31a7
Aryan <aryanarora2285@gmail.com>
2022-07-18 19:20:38 -0700

    updated style on button click

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -5,6 +5,7 @@ import { incrementQuantity } from "../app/reducers/ProductSlice";
 export default function AddButton({id}){
     const dispatch = useDispatch();
     return (
-        <button className="add-button" onClick = {(e) => {e.stopPropagation(); e.preventDefault(); dispatch(incrementQuantity(id))}}>Add to Cart</button>
+        <button className="add-button" onClick = {(e) => {e.stopPropagation(); e.preventDefault(); dispatch(incrementQuantity(id)); e.target.style.backgroundColor = "red"; e.target.style.backgroundColor = "white"; e.target.style.color = "#00E9A3"; e.target.style.border = "2px solid #00E9A3"}}
+        onMouseLeave = {(e) => {e.target.style.backgroundColor = "#00E9A3"; e.target.style.color = "white"}}>Add to Cart</button>
     )
 }
